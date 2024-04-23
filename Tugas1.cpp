ce7550dcb1f637af7559463030f6e5ba8093c7ff
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2024-04-23 14:53:56 +0700

    menambahkan void output

--- Tugas1.cpp
+++ Tugas1.cpp
@@ -28,6 +28,12 @@ class Matakuliah{
             cout << "SKS : ";
             cin >> sks;
         }
+
+        void output(){
+            cout << "Kode MK : " << kodeMK << endl;
+            cout << "Nama MK : " << namaMK << endl;
+            cout << sks;
+        }
 };
 
 int main () {
